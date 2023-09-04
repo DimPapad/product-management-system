199f932a255727918f7e3d92b83116bed46cb9b2
Dimitris Papado <papadogd@gmail.com>
2023-09-04 23:57:08 +0300

    Dimitris' initial commit

---
+++ src/test/java/com/example/productmanagementsystem/ProductManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
